58f694995b53e4d66f1e2dfef3913bfe0221f708
Felipe Lyra <fgmlyra@gmail.com>
2023-03-16 23:52:03 -0500

    added bit or struckture

---
+++ script.js
@@ -0,0 +1,23 @@
+var clockTimer
+var startTimer = document.querySelector("#startTimer")
+    
+    clockTimer = (function() {
+    var sec = 60;
+    function startTimer(){
+        console.log('timer suppose to go')
+        var timer = setInterval(function(){
+            sec--;
+            document.getElementById('timerDisplay').innerHTML='00:'+sec;
+            if (sec < 0) {
+                clearInterval(timer);
+                alert("Time is up!")
+            }
+        }, 1000);
+    }
+    document.getElementById
+    document.getElementById('incorrect').addEventListener('click', function() {
+        sec -= 5;
+        document.getElementById('timerDisplay').innerHTML='00:'+sec;
+    });
+    startTimer();
+})();
